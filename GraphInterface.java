5e25d4f95de3b6b33d74a2bd515b33652073f9c2
ISIN <isinmin@mail.ru>
2017-06-28 00:18:16 +0300

    Документация
    + комментарии
    + документация
    + описание параметров

--- GraphInterface.java
+++ GraphInterface.java
@@ -1,6 +1,14 @@
 
 public interface GraphInterface {
 
+//удаляет все вершины и рёбра из графа
     void clear();
+
+/**
+*добавление ребра в граф
+* @param _x      начало дуги
+* @param _y      конец дуги
+* @param _weight вес дуги
+*/
     void addEdge(int _x, int _y, int _weight);
 }
